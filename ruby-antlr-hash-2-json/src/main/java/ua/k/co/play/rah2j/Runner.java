6334811c2b98e13a9ed042fb044ef304638cbaec
Vasyl Khrystiuk <vasyl.khrystiuk@ticketmaster.com>
2019-06-26 03:29:36 +0300

    working programm

--- ruby-antlr-hash-2-json/src/main/java/ua/k/co/play/rah2j/Runner.java
+++ ruby-antlr-hash-2-json/src/main/java/ua/k/co/play/rah2j/Runner.java
@@ -1,9 +1,39 @@
 package ua.k.co.play.rah2j;
 
 
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+
+import java.io.IOException;
+
 public class Runner {
-    public static void main(String[] args) {
-        System.out.println("hello world!");
+    public static void main(String[] args) throws IOException {
+
+        // create a CharStream that reads from standard input
+        CharStream input = CharStreams.fromStream(System.in);
+
+        // create a lexer that feeds off of input CharStream
+        ArrayInitLexer lexer = new ArrayInitLexer(input);
+
+        // create a buffer of tokens pulled from the lexer
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+
+        // create a parser that feeds off the tokens buffer
+        ArrayInitParser parser = new ArrayInitParser(tokens);
+        ParseTree tree = parser.init(); // begin parsing at init rule
+
+
+        // Create a generic parse tree walker that can trigger callbacks
+        ParseTreeWalker walker = new ParseTreeWalker();
+        // Walk the tree created during the parse, trigger callbacks
+        walker.walk(new ShortToUnicodeString(), tree);
+
+        System.out.println(); // print a \n after translation
+
     }
 
     public int add(int a, int b) {
